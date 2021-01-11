28d33832c7f82b35d5ed2a59ec596f1c7e459871
azharkhan1 <azhar40@live.co.uk>
2021-01-11 13:02:55 +0500

    list comprehension and tuple

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,19 @@
-print ("Hello World")
+mySquare = []
+for num in range(10):
+    square = num ** 2
+    mySquare.append(square)
+mySquare
+
+#code Compact
+newSquareLIst = [num**2 for num in range(10)]
+newSquareLIst
+
+
+
+["div by 2" if num%2==0 else "not even" for num in range(10)]
+[f"{num} even" if num%2==0 else f"{num} odd" for num in range(10)]
+
+
+a = 23,24 #packing
+b,c = a
+
